22b5d507192514952dbe11cd6cdf6a56a502c3a1
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-03-19 10:57:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 from machine import Pin
 from time import sleep
+import os
+import gc
+import WifiConnect
 
-# __INIT_ROUTINE__
-led = Pin(2, Pin.OUT)
-led.value(1)
-sleep(0.5)
-led.value(0)
-    
-# __WIFI_CONNECTION__    
-''' 
-  define a function to connect wifi
-  call the func if the connection is unsuccesfull
-  try 5 times and then continue
-'''
 
-import network
-sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
-sta_if.connect(os.environ.get('SSID'), os.environ.get('SSIDPW'))
-if sta_if.isconnected():
-  for i in range (3):
-    Pin(2, Pin.OUT).value(1)
-    sleep(0.2)
-    Pin(2, Pin.OUT).value(0)
-    sleep(0.2)
+def init_routine():
+    ''' __INIT_ROUTINE__
+        Just a phisical indication of the main script running with onboard Led
+    '''
+    led = Pin(2, Pin.OUT)
+    led.value(1)
+    sleep(0.5)
+    led.value(0)
     
-while True:
-    Pin(2, Pin.OUT).value(1)
-    sleep(1)
-    Pin(2, Pin.OUT).value(0)
-    sleep(1)
+def main():
+    ConnectWifi()
+    while True:
+        Pin(2, Pin.OUT).value(1)
+        sleep(1)
+        Pin(2, Pin.OUT).value(0)
+        sleep(1)
+        
+if __name__ == "__main__":
+    init_routine_routine()
+    main()
