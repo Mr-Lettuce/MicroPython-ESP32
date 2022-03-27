0ed47ad1920001b9d9d1572143b61bbeeabb7c8d
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-03-27 11:45:42 -0300

    "Merge" with Let32 Old Repo

--- WifiConnect.py
+++ WifiConnect.py
@@ -1,13 +1,15 @@
 import network
-import time
 import os
+import gc
 from machine import Pin
+from time import sleep
+
 
 def ConnectWifi():
   ''' To set up the SSID and SSID Password 'os' module must be reinstalled to get the option " os.environ['SSID'] = 'YourSSID' "
   Those environment variables can be set in the ' boot.py ' file
   '''
-  print('Running WifiConnect.py, updated with commit f6b1302')
+  print('Running WifiConnect.py, updated with commit a9f7c16')
   led = Pin(2, Pin.OUT)
   sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
   if not sta_if.isconnected():
@@ -20,6 +22,7 @@ def ConnectWifi():
       sleep(0.2)
       Pin(2, Pin.OUT).value(0)
       sleep(0.2)
+  gc.collect()
   print(sta_if.ifconfig())
 
       
