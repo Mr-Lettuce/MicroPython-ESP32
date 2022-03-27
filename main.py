6518999aabf37c7583ccc1418cb2b02c585c9e55
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-03-27 11:40:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-from machine import Pin
-from time import sleep
 import os
 import gc
+import AioFunctions
 from WifiConnect import ConnectWifi
+from MiscFunctions import init_routine
+from MiscFunctions import Routine
+import MiscFunctions
 
 
-def init_routine():
-    ''' __INIT_ROUTINE__
-        Just a phisical indication of the main script running with onboard Led
-    '''
-    led = Pin(2, Pin.OUT)
-    led.value(1)
-    sleep(0.5)
-    led.value(0)
-    
 def main():
     ConnectWifi()
-    
-        
+    gc.collect()
+    routine0 = Routine()                # TODO: Make persistant trough saving setting into a file
+    #mqtt_connect()
+    #subscribe_to_feeds()
+   
 if __name__ == "__main__":
     init_routine()
     main()
