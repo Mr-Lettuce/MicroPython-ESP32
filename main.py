2bca6917db693cb67642dfc90de7dc9c3665b670
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-04-02 00:47:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import gc
 import AioFunctions
+import HwControl
 from WifiConnect import ConnectWifi
 from MiscFunctions import init_routine
 from MiscFunctions import Routine
@@ -8,12 +9,18 @@ import MiscFunctions
 
 
 def main():
+    '''
+    _TODO_
+    monitoring of wifi network connection & to reconnect
+    monitoring of MQTT connection & reconnect
+    make routine persistant trough saving settings into a db
+    '''
     ConnectWifi()
-    gc.collect()
-    routine0 = Routine()                # TODO: Make persistant trough saving setting into a file
-    #mqtt_connect()
-    #subscribe_to_feeds()
-   
+    routine0 = Routine()
+    _thread.start_new_thread(mqtt_connect_and_subscribe, ())
+    control_monitoring()
+    
+    
 if __name__ == "__main__":
     init_routine()
     main()
