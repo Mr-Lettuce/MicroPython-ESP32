efd9a9a22c9a599530756bb4d789426a4a3c7a0b
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-04-07 01:44:54 -0300

    working

--- WifiConnect.py
+++ WifiConnect.py
@@ -1,3 +1,5 @@
+#
+
 import network
 import os
 import gc
@@ -9,14 +11,13 @@ def ConnectWifi():
   ''' To set up the SSID and SSID Password 'os' module must be reinstalled to get the option " os.environ['SSID'] = 'YourSSID' "
   Those environment variables can be set in the ' boot.py ' file
   '''
-  print('Running WifiConnect.py, updated with commit a9f7c16')
   led = Pin(2, Pin.OUT)
   sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
   if not sta_if.isconnected():
     sta_if.connect(os.environ.get('SSID'), os.environ.get('SSIDPW'))
     print('Connecting to the wifi Network: ' + os.environ.get('SSID'))
     while not sta_if.isconnected():
-      time.sleep(1)
+      sleep(1)
     for i in range (3):
       Pin(2, Pin.OUT).value(1)
       sleep(0.2)
