9ccf4fb554c403d0facf08ff735fc404eb3bafe5
Daniel Brieva <65506898+Mr-Lettuce@users.noreply.github.com>
2022-04-12 01:34:43 -0300

    Updated PCB pins

--- boot.py
+++ boot.py
@@ -24,16 +24,18 @@ os.environ['AIO_SUB_FEED'] = [ 'esp-actions.cmd' ]
 os.environ['AIO_REP_FEED'] = [ 'reports.humidity', 'reports.temperature', 'esp-actions.summary']
 
 # __HARDWARE_ASSIGNMENT__
-os.environ['DHT_PIN']           = 39
-os.environ['WATER_PROBE_PIN']   = 34
-os.environ['SINK_PROBE_PIN']    = 35
-os.environ['PELT_PIN']          = 32
-os.environ['HEAT_PIN']          = 33
-os.environ['COOLER_PIN']        = 25
-os.environ['VENTILATION_PIN']   = 26
-os.environ['R_LED_PIN']         = 27
-os.environ['G_LED_PIN']         = 14
-os.environ['B_LED_PIN']         = 12
+os.environ['DHT_PIN']           = 11 #39 x
+os.environ['WATER_PROBE_PIN']   = 36 #34 -*
+os.environ['SINK_PROBE_PIN']    = 39 #35 -*
+os.environ['PELT_PIN']          = 33 #32 -
+os.environ['HEAT_PIN']          = 25 #33 -
+os.environ['COOLER_PIN']        = 35 #25 -
+os.environ['VENTILATION_PIN']   = 32 #26 -
+os.environ['LIGHT_PIN']         = 34 #   -
+os.environ['HUMID_PIN']         = 26 #   x
+os.environ['R_LED_PIN']         = 6  #27 
+os.environ['G_LED_PIN']         = 7  #14 
+os.environ['B_LED_PIN']         = 8  #12 
 
 '''
 Pines PWM
